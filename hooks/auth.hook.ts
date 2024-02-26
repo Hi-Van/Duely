ccffdd0b7f153836657bf3ddd8793689a0c3734f
Hi-Van <sirisoukvanouphon@gmail.com>
2024-02-26 11:15:17 -0600

    refactor: add health endpoint

--- hooks/auth.hook.ts
+++ hooks/auth.hook.ts
@@ -2,6 +2,10 @@ import useSWR from "swr";
 import { fetcher, FetchResponse } from "@/hooks/fetch";
 import api from "@/config/api.config.json";
 
+type HealthData = {
+  status: string | number;
+}
+
 type LoginBody = {
   email?: string;
   username: string;
@@ -23,6 +27,17 @@ type ValidationData = {
   isAuthorized: boolean;
 };
 
+export const useHealthCheck = (): FetchResponse<HealthData, Error> => {
+  const url = api.baseUrl + api.endpoints.auth.health;
+  const { data, error, isLoading } = useSWR(url, () => fetcher<HealthData, any, any>(url, "GET"));
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+};
+
 export const useLogin = (
   username: string,
   password: string
@@ -42,11 +57,31 @@ export const useLogin = (
 export const useRegistration = (
   username: string,
   password: string,
-  email: string,
+  email: string
 ): FetchResponse<LoginData, Error> => {
   const url = api.baseUrl + api.endpoints.auth.register;
   const { data, error, isLoading } = useSWR(url, () =>
-    fetcher<LoginData, LoginBody, any>(url, "POST", { username, password, email })
+    fetcher<LoginData, LoginBody, any>(url, "POST", {
+      username,
+      password,
+      email,
+    })
+  );
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+};
+
+export const useValidation = () => {
+  const url = api.baseUrl + api.endpoints.auth.validate;
+  const { data, error, isLoading } = useSWR(url, () =>
+    fetcher<ValidationData, any, ValidationHeaders>(url, "GET", undefined, {
+      authorization: localStorage.getItem("token") || "",
+      "user-id": localStorage.getItem("userId") || "",
+    })
   );
 
   return {
